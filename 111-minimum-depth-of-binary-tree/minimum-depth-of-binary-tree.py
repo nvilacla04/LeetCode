f631d0a9e7eea1464ec0efa9d5001806b9753341
Nicolau Vilaclara <nicolaurve@gmail.com>
2026-03-08 17:14:48 +0100

    Time: 7 ms (69.20%) | Memory: 44.5 MB (72.79%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -5,22 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minDepth(self, root: TreeNode) -> int:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
+        
         q = collections.deque([root])
         depth = 1
+        
         while q:
             qSize = len(q)
+            
             for _ in range(qSize):
                 node = q.popleft()
-                # Since we added nodes without checking null, we need to skip them here.
+                
                 if not node:
-                    continue
-                # The first leaf would be at minimum depth, hence return it.
+                    continue 
+                
                 if not node.left and not node.right:
                     return depth
+                
                 q.append(node.left)
                 q.append(node.right)
-            depth += 1
-        return -1
+                
+            depth += 1 
+        return -1 
